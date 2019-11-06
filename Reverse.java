719db870eac43198a8a1ff14e76e203d9cbe4af6
tlcode007 <56943951+tlcode007@users.noreply.github.com>
2019-11-05 20:59:55 -0600

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,22 @@
+package com.edgetechHomeWork;
+
+public class Reverse {
+    public static void main(String[] args) {
+        String name ="My name is Tyra";
+       name.length();
+        System.out.println(name.length());
+        StringBuilder sb = new StringBuilder("level, a man a plan a canal panama");
+        System.out.println("string = " + sb);
+        // reversing of stringbuilder
+        System.out.println("reverse = " + sb.reverse());
+
+        StringBuilder sb2 =new StringBuilder("go hang a salami I'm a lasagna hog");
+        System.out.println("string =" + sb2);
+        System.out.println("reverse = " +sb2.reverse());
+
+    }
+
+        }
+
+
+
